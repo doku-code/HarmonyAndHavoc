056e812e0d70f1eade312c57b0df94f8bab8bab3
Aymerick Fievet <129483516+Nithlian@users.noreply.github.com>
2023-10-17 19:27:56 -0400

    ++Singletons Base
    reviewer : Jean Francois Major

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace AF
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+        
+        void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }\
+    }
+}
