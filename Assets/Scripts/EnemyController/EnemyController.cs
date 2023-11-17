6ad741b6f86a96ae185bc523d79d9b14376984db
Charles Dionne <charlesdionne@mailo.com>
2023-11-17 01:05:51 -0500

    EnemyController
    Reviewer : Jf-Major

--- Assets/Scripts/EnemyController/EnemyController.cs
+++ Assets/Scripts/EnemyController/EnemyController.cs
@@ -1,6 +1,7 @@
 using AF;
 using UnityEngine;
 using JFM;
+using System.Collections;
 
 namespace charles
 {
@@ -8,9 +9,7 @@ namespace charles
     {
         public int maxHealth = 100;
         public int attackDamage = 10;
-        public int defense = 5;
 
-        [SerializeField] private PlayerData playerData;
         private Animator npcAnimator;
         private int currentHealth;
 
@@ -24,25 +23,13 @@ namespace charles
         {
             if(collision.CompareTag("Player"))
             {
-                //Attack(playerData);
                 Attack(collision.gameObject.GetComponent<PlayerController>(), Vector2.right * Mathf.Sign(collision.transform.position.x - transform.position.x));
                 Debug.Log("this is a CRITICAL HIT");
             }
         }
         public void TakeDamage(int damage)
         {
-            
-            /* ? */
-            int actualOrder = playerData.ActualOrder;
-
-            /* ? */
-            int armorUpgrade = playerData.ArmorUpgrade;
-
-            /* ? */
-            int damageTaken = Mathf.Max(0, damage - (defense + armorUpgrade));
-
-            currentHealth -= damageTaken;
-
+            currentHealth -= Mathf.Max(0, damage);
             npcAnimator.SetTrigger("GetHit");
 
             if (currentHealth <= 0)
@@ -54,16 +41,21 @@ namespace charles
 
         public void Attack(PlayerController player, Vector2 direction)
         {
-            /*int armorUpgrade = playerData.ArmorUpgrade;
-            int damageToPlayer = Mathf.Max(0, attackDamage - armorUpgrade);
-            playerData.TakeDamage(damageToPlayer);
-            */
             player.TakeDamage(attackDamage, direction);
         }
 
         private void Die()
         {
-          Destroy(gameObject);
+            npcAnimator.SetTrigger("Death");
+            StartCoroutine(DestroyAfterAnim(npcAnimator.GetCurrentAnimatorStateInfo(0).length));
+        }
+
+        private IEnumerator DestroyAfterAnim(float waitTime)
+        {
+            
+            yield return new WaitForSeconds(waitTime);
+            Destroy(gameObject);
+
         }
     }
 }
