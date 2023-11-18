77babe2e3d43ba5280d0eeba11ef133db2dcac41
Aymerick Fievet <129483516+Nithlian@users.noreply.github.com>
2023-11-17 23:47:02 -0500

    Update InventoryManager.cs

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -13,21 +13,20 @@ namespace AF
 
         [Space]
         [Header("Knowledge Buttons"), Tooltip("All the Knowledge Button from the inventory UI")]
-        [SerializeField]
-        private Button knowledge1;
-
+        [SerializeField] private Button knowledge1;
         [SerializeField] private Button knowledge2;
         [SerializeField] private Button knowledge3;
         [SerializeField] private Button knowledge4;
         [SerializeField] private Button knowledge5;
         [SerializeField] private Button knowledge6;
         [SerializeField] private Button knowledge7;
-        [Space] [SerializeField] private Image disabledKnowledgeImage;
+        [Space] 
+        [SerializeField] private Image disabledKnowledgeImage;
+        [Space]
         [SerializeField] private GameObject InventoryPanel;
         [SerializeField] private GameObject PauseMenuPanel;
         private bool isInventoryMenuOpen = false;
-
-
+        
         public void InventoryMenu(CallbackContext value)
         {
             if (value.performed)
