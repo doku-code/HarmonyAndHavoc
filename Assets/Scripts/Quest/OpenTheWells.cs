d21e9109865e1b5dae9fc717f61c80fa656f4d87
Charles Dionne <charlesdionne@mailo.com>
2023-11-25 18:20:36 -0500

    hh-143 refaire ENCORE le ui

--- Assets/Scripts/Quest/OpenTheWells.cs
+++ Assets/Scripts/Quest/OpenTheWells.cs
@@ -5,11 +5,19 @@ namespace charles
     public class OpenTheWells : MonoBehaviour
     {
         [SerializeField] private GameObject wellsCollider;
-
+        private bool wellsIsOpen = false;
         public void onAcceptQuest()
         {
+            if (!wellsIsOpen)
+            {
             SoundManager.Instance.PlayAClip(4);
             wellsCollider.SetActive(false);
+                wellsIsOpen = true;
+            }
+            else
+            {
+                wellsCollider.SetActive(false);
+            }
         }
     }
 }
