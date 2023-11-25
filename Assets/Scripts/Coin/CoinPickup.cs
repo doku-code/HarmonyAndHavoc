307eb4ffa3dafe64a43a69756f8bd6d8493d7f89
Jean-François Major <jf.major@hotmail.com>
2023-11-25 16:18:04 -0500

    *Coin pickup.

--- Assets/Scripts/Coin/CoinPickup.cs
+++ Assets/Scripts/Coin/CoinPickup.cs
@@ -1,17 +1,28 @@
 using AF;
+using JFM;
 using UnityEngine;
 
 namespace charles
 {
-    public class CoinPickup : MonoBehaviour
+    public class CoinPickup : MonoBehaviour, IInteractible
     {
         void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player") && other is CapsuleCollider2D)
             {
                 SoundManager.Instance.PlayAClip(3);
-               gameObject.SetActive(false);
+                gameObject.SetActive(false);
             }
+        }
+
+        public void Interact(GameObject gameObject) 
+        {
+            PlayerController pc = null;
+            
+            if(gameObject.TryGetComponent<PlayerController>(out pc))
+            {
+                pc.Data.Gold++;
+            }            
         }
     }
 }
--- Assets/Scripts/Interfaces/IInteractible.cs
+++ Assets/Scripts/Interfaces/IInteractible.cs
@@ -1,7 +1,9 @@
+using UnityEngine;
+
 namespace AF
 {
     public interface IInteractible
     {
-        public void Interact() { }
+        public void Interact(GameObject gameObject) { }
     }
 }
