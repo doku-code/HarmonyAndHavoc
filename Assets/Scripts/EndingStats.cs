0be4e07c65c59f8bdb51fc6d91b94323fbd4da3c
Charles Dionne <87602090+Spicycurry1212@users.noreply.github.com>
2023-12-11 16:03:55 -0500

    Push village + Lvl2

--- Assets/Scripts/EndingStats.cs
+++ Assets/Scripts/EndingStats.cs
@@ -2,6 +2,8 @@ using AF;
 using JFM;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace charles
 {
@@ -18,16 +20,60 @@ namespace charles
         [SerializeField] private TMP_Text weaponUpgrade;
         [SerializeField] private TMP_Text armorUpgrade;
         [SerializeField] private TMP_Text healAmount;
+        [SerializeField] private VideoPlayer video;
+        [Header("video")]
+        [SerializeField] private VideoClip noChaos;
+        [SerializeField] private VideoClip chaosOne;
+        [SerializeField] private VideoClip chaosTwo;
+        [SerializeField] private VideoClip chaosThree;
+        [Header("Render Texture")]
+        [SerializeField] private RawImage mp4Video;
+        [SerializeField] private RenderTexture noChaosRenderer;
+        [SerializeField] private RenderTexture chaosOneRenderer;
+        [SerializeField] private RenderTexture chaosTwoRenderer;
+        [SerializeField] private RenderTexture chaosThreeRenderer;
+
+
 
         private void Start()
         {
-            chaosLevel.text = "CHAOS LEVEL: " + "Not Yet implemented";
-            enemyDeathCount.text = "Enemy Death Count: " + "Not Yet implemented";
-            goldCollected.text = "Gold Collected: " + playerData.Gold;
+            chaosLevel.text = "CHAOS LEVEL: " + ChaosOrderSystem.Instance.ChaosAmount;
+            enemyDeathCount.text = "Enemy Death Count: " + playerData.TotalKills;
+            goldCollected.text = "Gold Collected: " + playerData.TotalGold;
             weaponUpgrade.text = "Weapon Upgrade: " + playerData.WeaponUpgrade;
             armorUpgrade.text = "Armor Upgrade: " + playerData.ArmorUpgrade;
-            healAmount.text = "Total Heal: " + "Not yet Implemented";
+            healAmount.text = "Total Heal: " + playerData.TotalHeals;
+
+            SwitchVideoOnChaos();
         }
 
+        private void SwitchVideoOnChaos()
+        {
+            int chaosAmount = ChaosOrderSystem.Instance.ChaosAmount;
+
+            switch (chaosAmount)
+            {
+                case 0:
+                    mp4Video.texture = noChaosRenderer;
+                    video.clip = noChaos;
+                    video.targetTexture = noChaosRenderer;
+                    break;
+                case 1:
+                    mp4Video.texture = chaosOneRenderer;
+                    video.clip = chaosOne;
+                    video.targetTexture = chaosOneRenderer;
+                    break;
+                case 2:
+                    mp4Video.texture = chaosTwoRenderer;
+                    video.clip = chaosTwo;
+                    video.targetTexture = chaosTwoRenderer;
+                    break;
+                default:
+                    mp4Video.texture = chaosThreeRenderer;
+                    video.clip = chaosThree;
+                    video.targetTexture = chaosThreeRenderer;
+                    break;
+            }
+        }
     }
 }
