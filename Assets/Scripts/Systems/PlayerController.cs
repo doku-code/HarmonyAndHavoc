c3480a0672aa12b8536ad737e0c09682bb6c6f6b
Jean-François Major <jf.major@hotmail.com>
2023-12-12 20:47:51 -0500

    Update PlayerController.cs

--- Assets/Scripts/Systems/PlayerController.cs
+++ Assets/Scripts/Systems/PlayerController.cs
@@ -918,15 +918,15 @@ namespace JFM
             SetBeneathObjectInfo();
 
             stateMachine.Update();           
-        }       
-        
+        }
+
         private void InitializeKnowledges()
         {
             foreach (var knowledge in playerData.EveryKnowledgeDictionary)
             {
                 knowledge.Value.Initialize(this);
             }
-            
+
             playerData.KnownKnowledgeDictionary[KnowledgeID.DASH] = true;
             //playerData.KnownKnowledgeDictionary[KnowledgeID.WALL_SLIDE] = true;
             //playerData.KnownKnowledgeDictionary[KnowledgeID.DOUBLE_JUMP] = true;
@@ -944,11 +944,20 @@ namespace JFM
             //playerData.GetKnowledgeByID(KnowledgeID.WALL_SLIDE).Activate();
             //playerData.GetKnowledgeByID(KnowledgeID.DOUBLE_JUMP).Activate();
             //playerData.GetKnowledgeByID(KnowledgeID.GROUND_SLIDE).Activate();
-            
+
             //playerData.GetKnowledgeByID(KnowledgeID.COMBO_ATTACK).Activate();
             //playerData.GetKnowledgeByID(KnowledgeID.AOE_ATTACK).Activate();            
-        }
 
+            // Re-activate equipped knowledges
+            foreach (var knowledge in playerData.EveryKnowledgeDictionary)
+            {
+                if (playerData.AvailableKnowledgeDictionary[knowledge.Key] 
+                    != AvailableKnowledgePosition.NOT_AVAILABLE)
+                {
+                    knowledge.Value.Activate();
+                }
+            }
+        }
         private void InputSetup()
         {
             SetInputDictionnary();
