68b9c3e8a3d70e8407df38a384769ff30a070e1a
Charles Dionne <87602090+Spicycurry1212@users.noreply.github.com>
2023-12-15 13:19:05 -0500

    add that when you pickup frag you get your health back

--- Assets/Scripts/Pickup_Order_Fragment.cs
+++ Assets/Scripts/Pickup_Order_Fragment.cs
@@ -15,6 +15,7 @@ namespace charles
                 playerData.OrderFragments += 1;
                 gameObject.SetActive(false);
                 SoundManager.Instance.PlayFxClip(clip);
+                playerData.ActualOrder = playerData.MaxOrder;
             }
         }
     }
